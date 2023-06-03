11597a9eb51c33c685fa56699be9cb50d24f6e5e
sudhirv <sudhirv@umich.edu>
2023-06-03 17:34:07 -0400

    Completed the HTML and CSS

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll('[date-number]');
+const operationButtons = document.querySelectorAll("[date-operation]");
+const equalsButton = document.querySelector("[date-equals]");
+const deleteButton = document.querySelector("[date-delete]");
+const allClearButton = document.querySelector("[data-all-clear]");
+const previousOperandTextElement = document.querySelector("[data-previous-operand]");
+const currentOperandTextElement = document.querySelector("[data-current-operand]");
